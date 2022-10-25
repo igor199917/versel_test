3f4f3fb23fd86fa023d247ac1d9ba10358b2bdee
igor <igor199917@inbox.ru>
2022-10-25 18:30:18 +0300

    init

---
+++ helpers/candyMachineInfo.js
@@ -0,0 +1,19 @@
+export const candyMachineAddress = "0x3b3a0901cfc91b289cd16b62b28fd850e52a2921d463945a3722c33a81bad453";
+export const collectionName = "TestCollection1232dodo"; // Case sensitive!
+export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmeCuKGLvAGrBAPGdaRh2hkTjXsXuk8VEUHNoYRD4cJG51";
+export const mode = "mainnet"; // "dev" or "test" or "mainnet"
+
+export let NODE_URL;
+export const CONTRACT_ADDRESS = "0xdf5c814388f4162f353e14f6123fcba8f39a958e4a2640e38e9e2c7cdfd2ac1d";
+export const COLLECTION_SIZE = 10
+let FAUCET_URL;
+if (mode == "dev") {
+    NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
+    FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+} else if (mode === "test") {
+    NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
+    FAUCET_URL = "https://faucet.testnet.aptoslabs.com";
+} else {
+    NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
+    FAUCET_URL = null;
+}
